cb32e45b5aff7a4060d2e8ccb824fbda9874f1b9
Andrey Zharkikh <orbital.logistics@yandex.ru>
2026-09-24 21:22:01 +0300

    feat: add switch packages

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
+import { packages } from './packages';
 import './style.css';
+
+const packageButtons = document.querySelectorAll('.product__package');
+const article = document.querySelector('#product-article');
+const price = document.querySelector('#product-price');
+const oldPrice = document.querySelector('#product-old-price');
+
+packageButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const selectedPackage = packages[button.dataset.package];
+
+    packageButtons.forEach((item) => {
+      item.classList.remove('is-active');
+    });
+
+    button.classList.add('is-active');
+
+    article.textContent = selectedPackage.article;
+    price.textContent = selectedPackage.price;
+    oldPrice.textContent = selectedPackage.oldPrice;
+  });
+});
---
+++ src/packages/index.js
@@ -0,0 +1,22 @@
+export const packages = {
+  100: {
+    article: '01306',
+    price: '326,40 ₽',
+    oldPrice: '349,20 ₽',
+  },
+  500: {
+    article: '01307',
+    price: '1 432 ₽',
+    oldPrice: '1 646 ₽',
+  },
+  1000: {
+    article: '01308',
+    price: '2 064 ₽',
+    oldPrice: '2 592 ₽',
+  },
+  5000: {
+    article: '01309',
+    price: '6 320 ₽',
+    oldPrice: '8 710 ₽',
+  },
+};
